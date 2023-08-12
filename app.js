30a943ca255b21d33e8392ed1857d2309cb54eae
JuanJoseVillalobos <juanjosevillalobos.19@gmail.com>
2023-08-11 21:33:56 -0300

    Primera Version de Mercado Liebre

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require ("express")
+const app = express()
+const path = require("path")
+
+app.listen(3000, () => {
+    console.log("Server running on [localhost:3000]")
+})
+
+app.use(express.static('public'))
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname + "/views/home.html"))
+})
+app.get("/register", (req,res) =>{
+    res.sendFile(path.join(__dirname + "/views/register.html"))
+} )
